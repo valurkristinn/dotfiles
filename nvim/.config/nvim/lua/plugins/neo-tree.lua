6647997655e14f5f41844deae67d8e702fb15042
valurkristinn <vkstarkadarson@ossur.com>
2026-08-05 11:25:21 +0000

    update: delta + neo-tree autoexpand

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -9,9 +9,17 @@ return {
 		},
 		lazy = false,
 		config = function()
-			vim.keymap.set("n", "<leader>e", "<cmd>Neotree filesystem toggle current<cr>")
+			vim.keymap.set("n", "<leader>e", "<cmd>Neotree filesystem reveal toggle current<cr>")
 			vim.keymap.set("n", "<leader>bb", "<cmd>Neotree buffers toggle current<cr>")
 			vim.keymap.set("n", "<leader>gg", "<cmd>Neotree git_status toggle current<cr>")
+			require("neo-tree").setup({
+				filesystem = {
+					follow_current_file = {
+						enabled = true,
+						leave_dirs_open = false,
+					},
+				},
+			})
 		end,
 	},
 }
