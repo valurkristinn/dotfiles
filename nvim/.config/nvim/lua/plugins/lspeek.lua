02cd4c3a8c2b21da29b2993dbadc1f2b5e1d2ee2
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-09-10 13:26:01 +0000

    update: branch in tmux statusbar + lspseek

---
+++ nvim/.config/nvim/lua/plugins/lspeek.lua
@@ -0,0 +1,9 @@
+return {
+	"r4ppz/lspeek.nvim",
+	commit = "3322ffb55deba02d47503309130b4a0164f79483",
+	keys = {
+		{ "gpd", "<cmd>LSPeekDef<CR>", desc = "Peek definition" },
+		{ "gpt", "<cmd>LSPeekTypeDef<CR>", desc = "Peek type definition" },
+	},
+	opts = {},
+}
